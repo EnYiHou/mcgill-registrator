916727d84da2fcfed2b03bec3e9354ec09cec215
EnYi Hou <enyi.hou@gmail.com>
2025-01-12 02:47:41 -0500

    add functionality for multiple course registration

--- script.py
+++ script.py
@@ -3,55 +3,84 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 import time
+from enum import Enum
 
-winter = "01"
-summer = "05"
-fall = "09"
+
+class Course:
+    class Season(Enum):
+        WINTER = "01"
+        SUMMER = "05"
+        FALL = "09"
+
+    def __init__(self, year, season, subject, course_number, type):
+        self.year = year
+        self.season = season
+        self.subject = subject
+        self.course_number = course_number
+        self.type = type
+        self.term = str(year) + season.value
+
+    def __str__(self):
+        return f"{self.subject} {self.course_number} ({self.type}) - {self.term}"
 
 
 # Variables to be set by the user
-#----------------------------------
-#----------------------------------
+# ----------------------------------
+# ----------------------------------
 
 student_id = "username"
 password = "password"
 
-year = 2025
-season = winter
-subject = "PHYS"
-course_number = "183"
-type = "Lecture"
+courses = [
+    Course(2025, Course.Season.WINTER, "PHYS", "183", "Lecture"),
+    Course(2025, Course.Season.WINTER, "COMP", "250", "Lecture"),
+    Course(2025, Course.Season.WINTER, "COMP", "322", "Lecture"),
+]
 
-#----------------------------------
-#----------------------------------
+# ----------------------------------
+# ----------------------------------
 
 
-term = str(year) + season
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=options)
 
-def register(CRN): 
+
+def register(CRN):
     print("Attempting to register...")
-    register_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit'][name='ADD_BTN'][value*='Register']")
+    register_button = driver.find_element(
+        By.CSS_SELECTOR, "input[type='submit'][name='ADD_BTN'][value*='Register']"
+    )
     register_button.click()
 
     CRN_input = driver.find_element(by=By.ID, value="crn_id1")
     CRN_input.send_keys(CRN)
 
-    submit_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit'][name='REG_BTN'][value*='Submit Changes']")
+    submit_button = driver.find_element(
+        By.CSS_SELECTOR, "input[type='submit'][name='REG_BTN'][value*='Submit Changes']"
+    )
     submit_button.click()
 
-    print("Registration attempt completed.")
 
+def check_registration(title):
+    try:
+        schedule_table = driver.find_element(
+            By.CSS_SELECTOR, "table.datadisplaytable[summary='Current Schedule']"
+        )
+        schedule_rows = schedule_table.find_elements(By.TAG_NAME, "tr")
+        for row in schedule_rows:
+            columns = row.find_elements(By.TAG_NAME, "td")
+            if columns and title in columns[10].text:
+                return True
+        return False
+    except:
+        return False
 
 
 def login():
     driver.get("https://horizon.mcgill.ca/pban1/twbkwbis.P_WWWLogin")
-
-
 
     username_input = driver.find_element(by=By.NAME, value="sid")
     username_input.send_keys(student_id)
@@ -73,15 +102,25 @@ def is_logged_in():
         return True
 
 
+def check_availability(course: Course):
 
-def check_availability():
+    term = course.term
+    subject = course.subject
+    type = course.type
+    course_number = course.course_number
+
     term_selection_url = "https://horizon.mcgill.ca/pban1/bwskfcls.p_sel_crse_search"
 
     driver.get(term_selection_url)
     time.sleep(1)
-    
+
     if not is_logged_in():
         login()
+
+    if not is_logged_in():
+        print("Login failed.")
+        driver.quit()
+        exit()
 
     driver.get(term_selection_url)
 
@@ -89,12 +128,15 @@ def check_availability():
     select = Select(term_selection_input)
     select.select_by_value(term)
 
-    submit_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit'][value*='Submit']")
+    submit_button = driver.find_element(
+        By.CSS_SELECTOR, "input[type='submit'][value*='Submit']"
+    )
     submit_button.click()
 
-    advanced_search_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit'][value*='Advanced Search']")
+    advanced_search_button = driver.find_element(
+        By.CSS_SELECTOR, "input[type='submit'][value*='Advanced Search']"
+    )
     advanced_search_button.click()
-
 
     subject_input = driver.find_element(by=By.ID, value="subj_id")
     select = Select(subject_input)
@@ -103,10 +145,15 @@ def check_availability():
     course_number_input = driver.find_element(by=By.ID, value="crse_id")
     course_number_input.send_keys(course_number)
 
-    submit_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit'][name='SUB_BTN'][value*='Get Course Sections']")
+    submit_button = driver.find_element(
+        By.CSS_SELECTOR,
+        "input[type='submit'][name='SUB_BTN'][value*='Get Course Sections']",
+    )
     submit_button.click()
     try:
-        found_sections_table = driver.find_element(By.CSS_SELECTOR, "table.datadisplaytable")
+        found_sections_table = driver.find_element(
+            By.CSS_SELECTOR, "table.datadisplaytable"
+        )
     except Exception as e:
         print(f"No classes were found that meet your search criteria in term {term}")
         driver.quit()
@@ -116,32 +163,59 @@ def check_availability():
     for row in rows:
         columns = row.find_elements(By.TAG_NAME, "td")
 
-        if columns and len(columns) == 20: #ensure that we are fetching from the correct row
+        if (
+            columns and len(columns) == 20
+        ):  # ensure that we are fetching from the correct row
             crn = columns[1].text
             subj = columns[2].text
             crse = columns[3].text
             c_type = columns[5].text
+            title = columns[7].text
             remaining = columns[12].text
+            waitlist_capacity = columns[13].text
+            waitlist_remaining = columns[15].text
             if len(remaining.strip()) == 0 or c_type != type:
                 continue
-            print(f"CRN: {crn}, Course: {subj} {crse}, Type: {c_type}, Remaining: {remaining} seats")
+            print(
+                f"CRN: {crn}, Course: {subj} {crse}, Type: {c_type}, Remaining: {remaining} seats"
+                + (f", Waitlist: {waitlist_remaining} seats" if waitlist_capacity.strip() != "0" else "")
+            )
+
+            if waitlist_capacity.strip() != "0":
+                if int(waitlist_remaining) > 0:
+                    print("WAITLIST AVAILABLE")
+                    return crn, title, True
+
+                else:
+                    print("NO WAITLIST AVAILABLE")
+                    return None, None, False
 
             if int(remaining) > 0:
                 print("SEATS AVAILABLE")
-                return crn, True
-            
+                return crn, title, True
+
     print("NO SEATS AVAILABLE")
-    return None, False
+    return None, None, False
 
 
 if __name__ == "__main__":
     while True:
-        CRN, available = check_availability()
-        if available:
-            register(CRN)
+        print("________________________________________")
+        print("Checking for available courses...")
+        if len(courses) == 0:
+            print("Registration complete.")
             driver.quit()
-            break
-        else:
-            seconds = 20
-            print(f"No seats available, retrying in {seconds} seconds...")
-            time.sleep(seconds)
+            exit()
+        print(f"Remaining courses:\n\t{'\n\t'.join([str(course) for course in courses])}")
+        for course in courses:
+            print(f"\nChecking availability for {course}")
+            crn, title, available = check_availability(course)
+            if available:
+                register(crn)
+                if check_registration(title):
+                    print(f"Successfully registered for {title}")
+                else:
+                    print(f"Failed to register for {title}")
+                courses.remove(course)
+                break
+        time.sleep(5)
